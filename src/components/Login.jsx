76d930e457d9854fb77ede962e1f11cb18d02248
QuangVu-dev <tqvu97@gmail.com>
2024-10-27 20:53:47 +0700

    push FE

---
+++ src/components/Login.jsx
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { Box, CardMedia } from "@mui/material";
+
+import { Videos, ChannelCard } from ".";
+import { loginAPI, loginFacebookAPI } from "../utils/fetchFromAPI";
+import { toast } from "react-toastify";
+import ReactFacebookLogin from "react-facebook-login";
+
+const Login = () => {
+   const [channelDetail, setChannelDetail] = useState();
+   const [videos, setVideos] = useState(null);
+
+   const { id } = useParams();
+   const navigate = useNavigate();
+
+   useEffect(() => {}, []);
+
+   return (
+      <div className="p-5 " style={{ minHeight: "100vh" }}>
+         <div className=" d-flex justify-content-center">
+            <form className="row g-3 text-white">
+               <div className="col-md-12">
+                  <label htmlFor="inputEmail4" className="form-label">
+                     Email
+                  </label>
+                  <input type="email" className="form-control" id="email" />
+               </div>
+
+               <div className="col-md-12">
+                  <label htmlFor="inputEmail4" className="form-label">
+                     Password
+                  </label>
+                  <input className="form-control" id="pass" />
+               </div>
+               <div className="col-12">
+                  <button
+                     onClick={() => {
+                        let email = document.getElementById("email").value;
+                        let pass_word = document.getElementById("pass").value;
+                        console.log(`email:${email}; pass_word:${pass_word} `);
+                        loginAPI({ email, pass_word })
+                           .then((result) => {
+                              // result gồm message và data (access token)
+                              // tạo popup thông báo login thành công
+                              toast.success(result.message);
+                              // lưu access token trong local storage của browzer
+                              localStorage.setItem("LOGIN_USER", result.data);
+                              // chuyển hướng sang trang chủ sau khi login thành công
+                              navigate("/");
+                           })
+                           .catch((error) => {
+                              console.log("error from API login");
+                              toast.error(error.response.data.message);
+                           });
+                     }}
+                     type="button"
+                     className="btn btn-primary"
+                  >
+                     Login
+                  </button>
+                  <ReactFacebookLogin
+                     appId="1119396706272724"
+                     fields="name, email,picture"
+                     callback={(response) => {
+                        // console.log(response);
+                        let { id, email, name } = response;
+                        loginFacebookAPI({ id, email, name })
+                           .then((result) => {
+                              toast.success(result.message);
+                              localStorage.setItem("LOGIN_USER", result.data);
+                              navigate("/");
+                           })
+                           .catch((error) => {
+                              toast.error(error.response.data.message);
+                           });
+                     }}
+                  />
+               </div>
+            </form>
+         </div>
+      </div>
+   );
+};
+
+export default Login;
---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+
+export const BASE_URL = "http://localhost:8080";
+
+const options = {
+   params: {
+      maxResults: 50,
+   },
+   headers: {
+      token: localStorage.getItem("LOGIN_USER"),
+   },
+};
+
+export const fetchFromAPI = async (url) => {
+   const { data } = await axios.get(`${BASE_URL}/${url}`);
+
+   return data;
+};
+
+export const getListVideo = async () => {
+   const { data } = await axios.get(`${BASE_URL}/videos/get-videos`);
+   return data;
+};
+
+export const getType = async () => {
+   const { data } = await axios.get(`${BASE_URL}/videos/get-type`, options);
+   return data;
+};
+
+export const getVideoById = async (typeId) => {
+   const { data } = await axios.get(`${BASE_URL}/videos/get-video-type-by-id/${typeId}`);
+   return data;
+};
+
+export const registerAPI = async (payload) => {
+   const { data } = await axios.post(`${BASE_URL}/auth/register`, payload);
+   return data;
+};
+
+export const loginAPI = async (payload) => {
+   const { data } = await axios.post(`${BASE_URL}/auth/login`, payload);
+   return data;
+};
+
+export const loginFacebookAPI = async (newUser) => {
+   const { data } = await axios.post(`${BASE_URL}/auth/login-face`, newUser);
+   return data;
+};
