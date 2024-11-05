2fcdd929906470cb5bdcdcbab942e6217039aa67
QuangVu-dev <tqvu97@gmail.com>
2024-11-06 00:53:18 +0700

    session 13

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -3,98 +3,129 @@ import axios from "axios";
 export const BASE_URL = "http://localhost:8080";
 
 const options = {
-   params: {
-      maxResults: 50,
-   },
-   headers: {
-      token: localStorage.getItem("LOGIN_USER"),
-   },
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    token: localStorage.getItem("LOGIN_USER"),
+  },
 };
 
 export const axiosInstance = axios.create({
-   baseURL: `${BASE_URL}`,
+  baseURL: `${BASE_URL}`,
 });
 
 // thêm 1 interceptor để gắn access token vào header trước mỗi request
 axiosInstance.interceptors.request.use(
-   (config) => {
-      if (config.requiredAuth) {
-         const token = localStorage.getItem("LOGIN_USER");
-         if (token) {
-            config.headers["token"] = `${token}`;
-         }
+  (config) => {
+    if (config.requiredAuth) {
+      const token = localStorage.getItem("LOGIN_USER");
+      if (token) {
+        config.headers["token"] = `${token}`;
       }
-      return config;
-   },
-   (error) => {
-      return Promise.reject(error);
-   }
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
 );
 
 const extendToken = async () => {
-   let { data } = await axiosInstance.post(`/auth/extend-token`, {}, { withCredentials: true });
+  let { data } = await axiosInstance.post(
+    `/auth/extend-token`,
+    {},
+    { withCredentials: true }
+  );
 
-   // lưu newAccessToken mới vào localStorage
-   localStorage.setItem("LOGIN_USER", data.data);
-   return data.data;
+  // lưu newAccessToken mới vào localStorage
+  localStorage.setItem("LOGIN_USER", data.data);
+  return data.data;
 };
 
 // custom response để thực hiện flow refresh token
 axiosInstance.interceptors.request.use(
-   (response) => {
-      return response;
-   }, // khi response có status code là 2xx
-   async (error) => {
-      const originalRequest = error.config;
-      console.log("error and extend token: ", error);
+  (response) => {
+    return response;
+  }, // khi response có status code là 2xx
+  async (error) => {
+    const originalRequest = error.config;
+    console.log("error and extend token: ", error);
 
-      if (error.response.status === 401) {
-         // call API extend-token để tạo access token mới
-         const newAccessToken = await extendToken();
-         originalRequest.headers["token"] = newAccessToken;
-         return axiosInstance(originalRequest);
-      }
-   } // khi response có status code khác 2xx
+    if (error.response.status === 401) {
+      // call API extend-token để tạo access token mới
+      const newAccessToken = await extendToken();
+      originalRequest.headers["token"] = newAccessToken;
+      return axiosInstance(originalRequest);
+    }
+  } // khi response có status code khác 2xx
 );
 
 export const fetchFromAPI = async (url) => {
-   const { data } = await axios.get(`${BASE_URL}/${url}`);
+  const { data } = await axios.get(`${BASE_URL}/${url}`);
 
-   return data;
+  return data;
 };
 
 export const getListVideo = async () => {
-   const { data } = await axiosInstance.get(`${BASE_URL}/videos/get-videos`);
-   return data;
+  const { data } = await axiosInstance.get(`${BASE_URL}/videos/get-videos`);
+  return data;
 };
 
 export const getType = async () => {
-   const { data } = await axiosInstance.get(
-      `${BASE_URL}/videos/get-type`,
-      { requiredAuth: true },
-      options
-   );
-   return data;
+  const { data } = await axiosInstance.get(
+    `${BASE_URL}/videos/get-type`,
+    { requiredAuth: true },
+    options
+  );
+  return data;
 };
 
 export const getVideoById = async (typeId) => {
-   const { data } = await axiosInstance.get(`${BASE_URL}/videos/get-video-type-by-id/${typeId}`);
-   return data;
+  const { data } = await axiosInstance.get(
+    `${BASE_URL}/videos/get-video-type-by-id/${typeId}`
+  );
+  return data;
 };
 
 export const registerAPI = async (payload) => {
-   const { data } = await axiosInstance.post(`${BASE_URL}/auth/register`, payload);
-   return data;
+  const { data } = await axiosInstance.post(
+    `${BASE_URL}/auth/register`,
+    payload
+  );
+  return data;
 };
 
 export const loginAPI = async (payload) => {
-   const { data } = await axiosInstance.post(`${BASE_URL}/auth/login`, payload, {
+  const { data } = await axiosInstance.post(`${BASE_URL}/auth/login`, payload, {
+    withCredentials: true, // cho phép gửi và nhận cookie từ server
+  });
+  return data;
+};
+
+export const loginAsyncKeyAPI = async (payload) => {
+  const { data } = await axiosInstance.post(
+    `${BASE_URL}/auth/login-async-key`,
+    payload,
+    {
       withCredentials: true, // cho phép gửi và nhận cookie từ server
-   });
-   return data;
+    }
+  );
+  return data;
 };
 
 export const loginFacebookAPI = async (newUser) => {
-   const { data } = await axiosInstance.post(`${BASE_URL}/auth/login-face`, newUser);
-   return data;
+  const { data } = await axiosInstance.post(
+    `${BASE_URL}/auth/login-face`,
+    newUser
+  );
+  return data;
+};
+
+export const forgotPassAPI = async (email) => {
+  const { data } = await axiosInstance.post(
+    `${BASE_URL}/auth/forgot-password`,
+    email
+  );
+  return data;
 };
